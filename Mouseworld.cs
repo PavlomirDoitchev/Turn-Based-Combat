dc85d50c05b4cff986754bf481acd593267df0c0
PavlomirDoitchev <pavlomirdoitchev@gmail.com>
2025-12-07 17:18:36 +0200

    init

---
+++ Mouseworld.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Assets.Scripts
+{
+    public class Mouseworld : MonoBehaviour
+    {
+        private static Mouseworld instance;
+        [SerializeField] private LayerMask mouseplaneLayerMask;
+        private void Awake()
+        {
+            instance = this;
+        }
+        
+        public static Vector3 GetPosition() 
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, instance.mouseplaneLayerMask);
+            return raycastHit.point;
+        }
+    }
+}
---
+++ Unit.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Assets.Assets.Scripts
+{
+    public class Unit : MonoBehaviour
+    {
+        private Vector3 targetPosition;
+        [SerializeField] private float moveSpeed = 5f;
+        float stoppingDistance = 0.1f;
+        private void Start()
+        {
+            targetPosition = transform.position;
+        }
+        private void Update()
+        {
+            if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+            {
+                Vector3 moveDirection = (targetPosition - transform.position).normalized;
+                transform.position += moveDirection * Time.deltaTime * moveSpeed;
+                transform.forward = moveDirection;
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Move(targetPosition: Mouseworld.GetPosition());
+            }
+        }
+        private void Move(Vector3 targetPosition)
+        {
+            this.targetPosition = targetPosition;
+        }
+    }
+}
